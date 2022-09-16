c12f51c0306b40da6fb7f100964bc70c4a1064b4
Paulo Santos <102436341+pauloninja@users.noreply.github.com>
2022-09-16 09:18:09 -0300

    atualizar

---
+++ game.js/game.js
@@ -0,0 +1,18 @@
+/*
+-----------------------------------------------
+    VARIÁVEIS E FUNÇÕES
+
+*/
+
+//LISTA COM OS INVASORES
+let invasores = document.getElementsByClassName
+
+
+//LISTA COM OS "BONZINHOS"
+
+
+/* 
+--------------------------------
+EVENTOS E EXECUÇÕES AUTOMÁTICAS 
+--------------------------------
+*/
