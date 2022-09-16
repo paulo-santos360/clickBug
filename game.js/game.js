93f08097c108f0f8da11368372755d7b0b3ed393
Paulo Santos <102436341+pauloninja@users.noreply.github.com>
2022-09-16 10:40:28 -0300

    atualizar

--- game.js/game.js
+++ game.js/game.js
@@ -14,12 +14,28 @@ let bonzinhos = document.getElementsByClassName('bonzinho')
 
 
 let score = 0
+
 let tempoRestante = 30
 
 let larguraQuadro = document.getElementById('quadro').offsetWidth
 console.log(larguraQuadro)
+//declaração global para as variáveis posX e posY (posicionamento)
+let posX, posY
+
+//Função para posicionar um elemento
+//recebe parâmetro el que informa o elemento
+const posicElemento = (el) => {
+    posX = Math.floor(Math.random()*1000)
+    posY = Math.floor(Math.random()*400)
+    el.style.position = 'absolute'
+    el.style.left = posX+'px'
+    el.style.top = posY+'px'
+
+}
 /* 
 --------------------------------
 EVENTOS E EXECUÇÕES AUTOMÁTICAS 
 --------------------------------
 */
+
+posicElemento(document.getElementById('inv1'))
