11e914d47f9157b055df8abc79ec95e3103f53a5
Paulo Santos <102436341+pauloninja@users.noreply.github.com>
2022-09-16 09:38:15 -0300

    atualizar

--- game.js/game.js
+++ game.js/game.js
@@ -6,11 +6,18 @@
 
 //LISTA COM OS INVASORES
 let invasores = document.getElementsByClassName('invasor')
+console.log('invasor')
 
 
 //LISTA COM OS "BONZINHOS"
+let bonzinhos = document.getElementsByClassName('bonzinho')
 
 
+let score = 0
+let tempoRestante = 30
+
+let larguraQuadro = document.getElementById('quadro').offsetWidth
+console.log(larguraQuadro)
 /* 
 --------------------------------
 EVENTOS E EXECUÇÕES AUTOMÁTICAS 
