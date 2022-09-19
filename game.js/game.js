defceb2b413b28dbb67cc55408f04ea42b0e022b
Paulo Santos <102436341+pauloninja@users.noreply.github.com>
2022-09-19 09:32:21 -0300

    Js

--- game.js/game.js
+++ game.js/game.js
@@ -46,31 +46,48 @@ const moveElemento = (el, veloc, inc)=> {
 
         veloc = veloc + inc
         el.style.left = veloc +'px'
-        //verifica se o elemento saiu do quadro e possui a
-        //classe emtela
-        //remove a classe e volta para uma posição inicial
+        //verifica se o elemento saiu do quadro 
+        //ou se foi clicado (classe "morto")
+        //retorna para uma posição 
+        //á esquerda do quadro (re-entra)
+        
+       
 
-        if(veloc > larguraQuadro && el.classList.cont('emtela')){
-            el.classList.remove('emtela')
+        if(veloc > larguraQuadro || el.classList.contains('morto')){
+           //sorteia um valor entre -
+            veloc = -Math.random()*450+50
+            inc = Math.random()*140+10
             posicElemento(el)
         }
     }, 40);
 }
 
 
-
-
-
-
-
-
+//ao clicar nos insetos
+const clickBug = (el)=>{
+    //Adiciona a classe "morto" ao inseto
+    el.classList.add('morto')
+    //Adiciona 10 pts ao score
+    score += 10
+    document.getElementById('score').innerText = score
+}
 
 
 
 for(inv of invasores){
     posicElemento(inv)
     moveElemento(inv, Math.random()*10, Math.random()*19+1)
+    inv.addEventListener('mousedown', (evt)=>{clickBug(evt.target)})
 }
 
 
-moveElemento(document.getElementById('inv1'), 5, 5)
+//moveElemento(document.getElementById('inv1'), 5, 5)
+
+
+/* var n1 = prompt("Digite um numero: ")
+n1 = parseFloat(n1)
+
+var n2 = prompt("Digite um numero: ")
+n2 = parseFloat(n2)
+
+alert(n1 + n2) */
